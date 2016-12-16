7fb60df42a6e8f28490a3605432bfa8964051a9f
jato <jatobrown@gmail.com>
2016-12-16 12:08:49 -0800

    initial logic started

--- js/atm.js
+++ js/atm.js
@@ -1,1 +1,33 @@
-atm_js.js
+function bankAccount(account, name, limit, blance) {
+  this.account = account;
+  this.name = name;
+  this.limit = limit;
+  this.balance = balance;
+  var amount = 0
+
+  this.getAccount = function() {
+    return this.account;
+  }
+
+  this.getLimit = function() {
+      return this.limit;
+  }
+
+  this.getBalance = function() {
+    return this.balance;
+  }
+
+  this.getName = function() {
+    return this.name;
+  }
+
+  this.withdraw_check = function(amount) {
+
+    this.can_charge = function(amount){
+    if(this.balance + amount < this.limit) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
